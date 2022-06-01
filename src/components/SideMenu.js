38c97b22e41e5bf86fb680cd7f3ebcd0da54df10
cdpaiva <cdpaivaa@gmail.com>
2022-06-01 13:20:19 -0400

    Phone layout

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -4,13 +4,15 @@ const SideMenu = ({ period, setPeriod }) => {
         <div className="side-menu">
             <div className="overlay">
                 <img className="img-avatar" src="/images/image-jeremy.png" alt="personal avatar" />
-                <p className="pre-text">Report for</p>
-                <h3 className="username">Jeremy Robson</h3>
+                <div className="avatar-content">
+                    <p className="pre-text">Report for</p>
+                    <h3 className="username">Jeremy Robson</h3>
+                </div>
             </div>
             <div className="interval-menu">
-                <p className={period==='daily' ? 'selected' : null} onClick={() => setPeriod('daily')}>Daily</p>
-                <p className={period==='weekly' ? 'selected' : null}onClick={() => setPeriod('weekly')}>Weekly</p>
-                <p className={period==='monthly' ? 'selected' : null}onClick={() => setPeriod('monthly')}>Monthly</p>
+                <p className={period === 'daily' ? 'selected' : null} onClick={() => setPeriod('daily')}>Daily</p>
+                <p className={period === 'weekly' ? 'selected' : null} onClick={() => setPeriod('weekly')}>Weekly</p>
+                <p className={period === 'monthly' ? 'selected' : null} onClick={() => setPeriod('monthly')}>Monthly</p>
             </div>
         </div>
     )
