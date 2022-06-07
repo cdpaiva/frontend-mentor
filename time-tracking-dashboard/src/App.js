8f03b38b7282f0201ca93e0743d62e21828e4235
cdpaiva <cdpaivaa@gmail.com>
2022-06-06 21:01:29 -0400

    File restructure

---
+++ time-tracking-dashboard/src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import SideMenu from './components/SideMenu'
+import Card from './components/Card'
+import dataSource from './data.json'
+
+function App() {
+  const [period, setPeriod] = useState('weekly')
+
+  return (
+    <div className="grid">
+      <SideMenu period={period} setPeriod={setPeriod}/>
+      {
+        dataSource.map(card => 
+        <Card 
+          period={period}
+          title={card.title}
+          timeframes={card.timeframes}
+          key={card.title}
+        />)
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ time-tracking-dashboard/src/components/SideMenu.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+
+const SideMenu = ({ period, setPeriod }) => {
+
+    return (
+        <div className="side-menu">
+            <div className="overlay">
+                <img className="img-avatar" src="/images/image-jeremy.png" alt="personal avatar" />
+                <div className="avatar-content">
+                    <p className="pre-text">Report for</p>
+                    <h3 className="username">Jeremy Robson</h3>
+                </div>
+            </div>
+            <div className="interval-menu">
+                <p className={period === 'daily' ? 'selected' : null} onClick={() => setPeriod('daily')}>Daily</p>
+                <p className={period === 'weekly' ? 'selected' : null} onClick={() => setPeriod('weekly')}>Weekly</p>
+                <p className={period === 'monthly' ? 'selected' : null} onClick={() => setPeriod('monthly')}>Monthly</p>
+            </div>
+        </div>
+    )
+}
+
+SideMenu.propTypes = {
+    period: PropTypes.string.isRequired,
+    setPeriod: PropTypes.func.isRequired
+}
+
+export default SideMenu
